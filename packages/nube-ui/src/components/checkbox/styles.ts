a4a7e05a732986e8f1ffd225c8a92d5fe7011d7c
imjivoi <jivoi001@gmail.com>
2023-08-14 20:42:27 -0300

    checkbox fixes

--- packages/nube-ui/src/components/checkbox/styles.ts
+++ packages/nube-ui/src/components/checkbox/styles.ts
@@ -3,9 +3,9 @@ import { tv } from 'tailwind-variants'
 export const checkbox = tv({
   slots: {
     root: 'flex items-center gap-2 cursor-pointer',
-    container: 'rounded-lg relative z-10 overflow-hidden transition-all duration-350 w-6 h-6 shadow-gray shadow-checkbox hover:bg-gray/40',
+    container: 'rounded-lg relative z-10 overflow-hidden transition-all duration-300 w-6 h-6 shadow-gray shadow-checkbox hover:bg-gray/40',
     checkmark: 'absolute w-full h-full',
-    checkmarkIcon: 'flex items-center justify-center transition-all duration-350 w-6 h-6 [&>span]:rotate-[45deg] [&>span]:block [&>span]:relative [&>span]:w-[8px] [&>span]:h-[13px] [&>span]:-mt-1',
+    checkmarkIcon: 'flex items-center justify-center transition-all duration-300 w-6 h-6 [&>span]:rotate-[45deg] [&>span]:block [&>span]:relative [&>span]:w-[8px] [&>span]:h-[13px] [&>span]:-mt-1',
     checkmarkFirstLine: 'absolute bg-transparent w-[8px] h-[1px] rounded-[5px] z-10 bottom-0 after:content-[""] after:absolute after:left-0 after:w-0 after:h-[2px] after:bg-white after:rounded-t-[5px] rounded-b-[5px]',
     checkmarkSecondLine: 'absolute bg-transparent w-[2px] h-[13px] rounded-[5px] bottom-0 right-0 after:content-[""] after:absolute after:left-0 after:bottom-0 after:w-[2px] after:h-0 after:bg-white after:rounded-t-[5px] rounded-r-[5px]',
     input: 'absolute inset-0 w-0 h-0 opacity-0 cursor-pointer',
